e31ac51f42fce150670cf441630b0667ca73dc94
Rexedead <2275788@gmail.com>
2017-06-25 05:05:52 +0300

    Add files via upload

---
+++ SteamWishlistTool.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author Rexedead, Hate
+// @name SteamWishListTool
+// @namespace steam-categories
+// @version 0.90
+// @description steam-categories
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_listValues
+// @grant       GM_deleteValue
+// @include *steamcommunity.com/id/*/wishlist*
+// @include *steamcommunity.com/profiles/*/wishlist*
+// @downloadURL https://raw.githubusercontent.com/Rexedead/SteamWishlistTool/master/SteamWishlistTool.user.js
+// @updateURL   https://raw.githubusercontent.com/Rexedead/SteamWishlistTool/master/SteamWishlistTool.meta.js
+// ==/UserScript==
