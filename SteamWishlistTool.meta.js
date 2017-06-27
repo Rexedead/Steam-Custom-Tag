1162980eba6dc9b7d0967f5dc265d29ce864b2b0
Rexedead <2275788@gmail.com>
2017-06-27 14:11:48 +0300

    Update SteamWishlistTool.meta.js

--- SteamWishlistTool.meta.js
+++ SteamWishlistTool.meta.js
@@ -2,13 +2,14 @@
 // @author Hate, Rexedead
 // @name SteamWishListTool
 // @namespace steam-categories
-// @version 0.99
+// @version 1.0
 // @description steam-categories
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_listValues
 // @grant       GM_deleteValue
-// @grant    GM_addStyle
+// @grant       GM_addStyle
+// @namespace https://github.com/Rexedead/SteamWishlistTool
 // @run-at document-end
 // @include *steamcommunity.com/id/*/wishlist*
 // @include *steamcommunity.com/profiles/*/wishlist*
