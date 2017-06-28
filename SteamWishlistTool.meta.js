a6117a960c1fe314b20ea8ad31b307b557d70ce2
HatemRise <ketchyp181995@mail.ru>
2017-06-28 12:15:22 +0400

    Update SteamWishlistTool.meta.js
    Version 1.2.1

--- SteamWishlistTool.meta.js
+++ SteamWishlistTool.meta.js
@@ -2,7 +2,7 @@
 // @author Hate, Rexedead
 // @name SteamWishListTool
 // @namespace steam-categories
-// @version 1.2
+// @version 1.2.1
 // @description steam-categories
 // @grant       GM_getValue
 // @grant       GM_setValue
