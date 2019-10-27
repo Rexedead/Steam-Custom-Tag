1542cceecda066093107c7fa1afa9d20ea8ca0a5
Rexedead <2275788@gmail.com>
2019-10-27 06:16:52 +0300

    Update SteamCustomTag.user.js

--- SteamCustomTag.user.js
+++ SteamCustomTag.user.js
@@ -19,7 +19,7 @@
 // @updateURL   https://github.com/Rexedead/Steam-Custom-Tag/raw/master/SteamCustomTag.meta.js
 // ==/UserScript==
 
-waitForKeyElements("#wishlist_ctn > div:nth-child(18)", main, true);
+waitForKeyElements("#wishlist_ctn > div:nth-child(1)", main, true);
 GM_addStyle(".customTagInput { white-space: nowrap; margin: 0; padding: 7px 10px; height: 20px; width: 100px; box-sizing: border-box;    background-color: rgba(0,0,0,0.1); border: 1px solid #000; color: #fff; border-radius: 3px; box-shadow: 0 0 3px rgba(0,0,0,0.5) inset, 1px 1px 0 0 rgba(255,255,255,0.2); margin-left:10px; } .customLabel{padding: 1px 10px 2px 10px; margin-right: 3px} .removeTagBtn{padding: 3px 10px 2px 10px; margin-right: 3px} @media screen and (max-width: 910px){.wishlist_row .platform_icons { width: 70%; pointer-events: auto;  }}");
 
 let gamesArr = GM_listValues();
