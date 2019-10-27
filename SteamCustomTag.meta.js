a54fcf556c8e422b7b4a95e774f7cffdedeca969
Rexedead <2275788@gmail.com>
2019-10-27 04:05:10 +0300

    Create SteamCustomTag.meta.js

---
+++ SteamCustomTag.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @author Rexedead
+// @name SteamCustomTag
+// @version 1.0
+// @description Organize Steam Wishlist Collection via tags
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_listValues
+// @grant       GM_deleteValue
+// @grant       GM_addStyle
+// @namespace https://github.com/Rexedead/Steam-Custom-Tag
+// @include *.steamcommunity.com/id/*/wishlist*
+// @include *.steamcommunity.com/profiles/*/wishlist*
+// @include *.steampowered.com/wishlist/id/*
+// @include *.steampowered.com/wishlist/profiles/*
+// @require  https://code.jquery.com/jquery-1.8.3.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @downloadURL https://github.com/Rexedead/Steam-Custom-Tag/raw/master/SteamCustomTag.user.js
+// @updateURL   https://github.com/Rexedead/Steam-Custom-Tag/raw/master/SteamCustomTag.meta.js
+// ==/UserScript==
